01a33824b2f29c00b6ecdb260e915316cf1092dc
Andre Lins <andrelins2403@hotmail.com>
2025-02-25 15:22:57 -0300

    first commit

---
+++ src/main/java/linsbr/dayofday/DayofdayApplication.java
@@ -0,0 +1,13 @@
+package linsbr.dayofday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayofdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayofdayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/linsbr/dayofday/DayofdayApplicationTests.java
@@ -0,0 +1,13 @@
+package linsbr.dayofday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayofdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
