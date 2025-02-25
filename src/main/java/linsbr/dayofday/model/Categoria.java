aa5a8367bea53e1c4607533c3a83fc31aa9a2754
Andre Lins <andrelins2403@hotmail.com>
2025-02-25 15:30:48 -0300

    feat(categoria): adiciona entidade categoria

---
+++ src/main/java/linsbr/dayofday/model/Categoria.java
@@ -0,0 +1,47 @@
+package linsbr.dayofday.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String descricao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
