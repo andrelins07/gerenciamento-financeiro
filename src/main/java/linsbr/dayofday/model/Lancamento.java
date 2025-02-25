1d7764a117c8637e4e1d1d17f3885f211672a062
Andre Lins <andrelins2403@hotmail.com>
2025-02-25 15:32:20 -0300

    feat(lancamento): adiciona a entidade lancamento

---
+++ src/main/java/linsbr/dayofday/model/Lancamento.java
@@ -0,0 +1,86 @@
+package linsbr.dayofday.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_lancamentos")
+
+public class Lancamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+    private LocalDate data;
+    private BigDecimal valor;
+
+    @ManyToOne
+    private Categoria categoria;
+
+    @ManyToOne
+    @JsonIgnoreProperties("lancamentos")
+    private Usuario usuario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Lancamento{" +
+                "id=" + id +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                ", valor=" + valor +
+                ", categoria=" + categoria +
+                ", usuario=" + usuario +
+                '}';
+    }
+}
