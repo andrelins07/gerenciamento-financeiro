00f0afed195db2a0102aef3c9c74aaeb7e351736
Andre Lins <andrelins2403@hotmail.com>
2025-02-25 22:49:49 -0300

    feat(controller): criacao dos endpoint para salvar, atualizar e deletar uma categoria

--- src/main/java/linsbr/dayofday/controller/CategoriaController.java
+++ src/main/java/linsbr/dayofday/controller/CategoriaController.java
@@ -1,16 +1,16 @@
 package linsbr.dayofday.controller;
 
+import jakarta.validation.Valid;
 import linsbr.dayofday.model.Categoria;
 import linsbr.dayofday.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -29,5 +29,34 @@ public class CategoriaController {
 
         return categoriaRepository.findById(id).map(ResponseEntity::ok)
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+    @PostMapping
+    public ResponseEntity<Categoria> cadastrarCategoria(@RequestBody @Valid Categoria categoria){
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
+
+    }
+
+    @PutMapping
+    public ResponseEntity<Categoria> atualizarCategoria(@Valid @RequestBody Categoria categoria) {
+
+        if (categoriaRepository.existsById(categoria.getId())) {
+            return ResponseEntity.status(HttpStatus.OK).body(categoriaRepository.save(categoria));
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deletarCategoria(@PathVariable Integer id) {
+
+        Optional<Categoria> categoria = categoriaRepository.findById(id);
+
+        if (categoria.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        categoriaRepository.deleteById(id);
     }
 }
--- src/main/java/linsbr/dayofday/model/Lancamento.java
+++ src/main/java/linsbr/dayofday/model/Lancamento.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
