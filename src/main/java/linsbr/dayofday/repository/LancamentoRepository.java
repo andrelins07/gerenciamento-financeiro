92c033e2fbd52829ffeb0b1e4a822d9542d27a98
Andre Lins <andrelins2403@hotmail.com>
2025-02-27 15:38:57 -0300

    feat(repository): criacao de query personalizada para verificar duplicidade de lancamento no sistema

--- src/main/java/linsbr/dayofday/repository/LancamentoRepository.java
+++ src/main/java/linsbr/dayofday/repository/LancamentoRepository.java
@@ -5,6 +5,8 @@ import linsbr.dayofday.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 
@@ -12,5 +14,8 @@ public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 
     @Query("SELECT l FROM Lancamento l JOIN FETCH l.categoria WHERE l.usuario IN :usuarios")
     List<Lancamento> findByUsuarioIn(List<Usuario> usuarios);
+
+    boolean existsByDescricaoAndValorAndDataAndUsuario(String descricao, BigDecimal valor, LocalDate data, Usuario usuario);
+
 }
 
