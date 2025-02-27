e70d03375867dec6c892f5741187eef456586efa
Andre Lins <andrelins2403@hotmail.com>
2025-02-27 13:48:05 -0300

    feat(repository): criacao de dois metodos de consultas personalizados

--- src/main/java/linsbr/dayofday/repository/CompartilhamentoPerfilRepository.java
+++ src/main/java/linsbr/dayofday/repository/CompartilhamentoPerfilRepository.java
@@ -1,7 +1,14 @@
 package linsbr.dayofday.repository;
 
 import linsbr.dayofday.model.CompartilhamentoPerfil;
+import linsbr.dayofday.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CompartilhamentoPerfilRepository extends JpaRepository<CompartilhamentoPerfil, Integer> {
+
+    List<CompartilhamentoPerfil> findByUsuarioCompartilhado(Usuario usuarioCompartilhado);
+
+
 }
--- src/main/java/linsbr/dayofday/repository/LancamentoRepository.java
+++ src/main/java/linsbr/dayofday/repository/LancamentoRepository.java
@@ -1,8 +1,16 @@
 package linsbr.dayofday.repository;
 
 import linsbr.dayofday.model.Lancamento;
+import linsbr.dayofday.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+
+    @Query("SELECT l FROM Lancamento l JOIN FETCH l.categoria WHERE l.usuario IN :usuarios")
+    List<Lancamento> findByUsuarioIn(List<Usuario> usuarios);
 }
+
